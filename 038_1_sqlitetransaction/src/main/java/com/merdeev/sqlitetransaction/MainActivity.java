2bbf9cff3c6398b955711e6f22c647773e1f4d9f
Ruslan Merdeev <rz.merdeev@itelma.su>
2017-09-18 15:30:20 +0400

    Added modules: 038_1_sqlitetransaction ... 039_1_sqliteonupgradedb

---
+++ 038_1_sqlitetransaction/src/main/java/com/merdeev/sqlitetransaction/MainActivity.java
@@ -0,0 +1,91 @@
+package com.merdeev.sqlitetransaction;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "States";
+    final String DB = "myDB";
+    final String TABLE = "mytable";
+
+    DBHelper dbh;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "--- onCreate Activity ---");
+        dbh = new DBHelper(this);
+        myActions();
+    }
+
+    void myActions() {
+        db = dbh.getWritableDatabase();
+        db.beginTransactionNonExclusive();
+        try {
+            delete(db, TABLE);
+            insert(db, TABLE, "val1");
+            read(db, TABLE);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+        } finally {
+            db.endTransaction();
+        }
+        dbh.close();
+    }
+
+    void delete(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Delete all from table " + table);
+        db.delete(table, null, null);
+    }
+
+    void insert(SQLiteDatabase db, String table, String value) {
+        Log.d(LOG_TAG, "Insert in table " + table + " value = " + value);
+        ContentValues cv = new ContentValues();
+        cv.put("val", value);
+        db.insert(table, null, cv);
+    }
+
+    void read(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Read table " + table);
+        Cursor c = db.query(table, null, null, null, null, null, null);
+        if (c != null) {
+            Log.d(LOG_TAG, "Records count = " + c.getCount());
+            if (c.moveToFirst()) {
+                do {
+                    Log.d(LOG_TAG, c.getString(c.getColumnIndex("val")));
+                } while (c.moveToNext());
+            }
+            c.close();
+        }
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            super(context, DB, null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            db.execSQL("create table " + TABLE + " ("
+                    + "id integer primary key autoincrement,"
+                    + "val text"
+                    + ");");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
